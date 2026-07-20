ad82b722e9c13519299d53c03e119922fb3ec1b3
hirahiragg <hirahira.mail@gmail.com>
2026-07-20 20:23:35 +0900

    Remove Go code and move Tauri project to root
    Delete all Go source files (cmd/, internal/, ui/, go.mod, go.sum)
    and move tauri-app/ contents to the top level.

--- internal/hotkey/focus_other.go
+++
@@ -1,7 +0,0 @@
-//go:build !darwin && !windows
-
-package hotkey
-
-func foregroundAppName() string { return "" }
-
-func isSelfFocused() bool { return false }
--- internal/hotkey/hotkey_other.go
+++
@@ -1,9 +0,0 @@
-//go:build !darwin && !windows
-
-package hotkey
-
-func Register(hotkeyStr string, onPress func()) error {
-	return nil
-}
-
-func Unregister() {}
